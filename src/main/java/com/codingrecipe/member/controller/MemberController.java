31c68cda5dbb525f80ac242f2a7b161f2a31a116
Minseo Han <readex11@naver.com>
2024-03-25 11:34:51 +0900

    memberList

--- src/main/java/com/codingrecipe/member/controller/MemberController.java
+++ src/main/java/com/codingrecipe/member/controller/MemberController.java
@@ -2,16 +2,20 @@ package com.codingrecipe.member.controller;
 
 import com.codingrecipe.member.dto.MemberDTO;
 import com.codingrecipe.member.service.MemberService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
-@RequiredArgsConstructor //
+@RequiredArgsConstructor
 public class MemberController {
     //생성자 주입
     @Autowired
@@ -27,7 +31,32 @@ public class MemberController {
     public String save(@ModelAttribute MemberDTO memberDTO){
         System.out.println("MemberController.save");
         System.out.println("memberDTO"+memberDTO);
+        //db 작업이 필요하면 service를 거친다.
         memberService.save(memberDTO);
         return "index";
+    }
+
+    @GetMapping("/member/login")
+    public String loginForm(){return "login";}
+
+    @PostMapping("/member/login")
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session){
+        MemberDTO loginResult=memberService.login(memberDTO);
+        if(loginResult!=null){
+            //로그인 성공
+            session.setAttribute("loginEmail", loginResult.getMemberEmail());
+            return "main";
+        }else{
+            //로그인 실패
+            return "login";
+        }
+    }
+
+    @GetMapping("/member/")
+    public String findAll(Model model){
+        List<MemberDTO> memberDTOList=memberService.findAll();
+        //어더한 html로 가져갈 데이터가 있다면 model사용
+        model.addAttribute("memberList", memberDTOList);
+        return "list";
     }
 }
--- src/main/java/com/codingrecipe/member/repository/MemberRepository.java
+++ src/main/java/com/codingrecipe/member/repository/MemberRepository.java
@@ -8,5 +8,7 @@ import java.util.Optional;
 //어떤 entity 클래스를 받을 것인가, entity 클래스의 pk 타입
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
     // 이메일로 회원 정보 조회 (select * from member_table where member_email=?)
-
+    //Optinal : null 방지
+    Optional<MemberEntity> findByMemberEmail(String memberEmail);
+    //모든 Repository에서 주고받는 객체들은 entity
 }
